0fbc961f25942f45dcc94d270ff11afa6febb4a7
asimonia <alex.simonian@gmail.com>
2017-03-05 12:39:51 -0600

    Using stacks to implement nested parens

---
+++ BasicDS/parens.py
@@ -0,0 +1,33 @@
+# Balanced parantheses: (()((())()))
+# Not balanced: ((((((())
+# Stacks can be used when dealing with expressions usings operators
+# and parentheses
+
+from pythonds.basic.stack import Stack
+
+def parChecker(symbolString):
+    s = Stack()
+    balanced = True
+    index = 0
+    while index < len(symbolString) and balanced:
+        symbol = symbolString[index]
+        if symbol in "([{":
+            s.push(symbol)
+        else:
+            if s.isEmpty():
+                balanced = False
+            else:
+                top = s.pop()
+                if not matches(top,symbol):
+                       balanced = False
+        index = index + 1
+    if balanced and s.isEmpty():
+        return True
+    else:
+        return False
+
+def matches(open,close):
+    opens = "([{"
+    closers = ")]}"
+    return opens.index(open) == closers.index(close)
+
