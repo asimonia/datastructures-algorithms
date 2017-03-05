e402e074aec9ab732bcd076ef2438f06c590d6b3
asimonia <alex.simonian@gmail.com>
2017-03-04 19:28:59 -0600

    Simplify fraction

--- Introduction/fraction.py
+++ Introduction/fraction.py
@@ -15,15 +15,32 @@ def gcd(m, n):
 class Fraction:
 
 	def __init__(self, top, bottom):
+		"""Initialize a fraction with a numerator and denominator"""
 		self.num = top
 		self.den = bottom
 
 	def __add__(self, otherfraction):
+		"""Override the add operator
+		Cross multiply.
+		Use the product of the two denominators as the common denominator.
+		Find the greatest common denominator to simply
+		the denominator and return the result.
+		"""
 		newnum = self.num * otherfraction.den + self.den * otherfraction.num
 		newden = self.den * otherfraction.den
 		common = gcd(newnum, newden)
 		return Fraction(newnum // common, newden // common)
 
 	def __str__(self):
-		return '{num}/{den}'.format(num=self.num, den=self.den)
+		"""Print out the fraction so it looks like 3/5"""
+		if self.den == 1:
+			return '{num}'.format(num=self.num)
+		else:
+			return '{num}/{den}'.format(num=self.num, den=self.den)
+
+	def __eq__(self, other):
+		firstnum = self.num * other.den
+		secondnum = other.num * self.den
+
+		return firstnum == secondnum
 
